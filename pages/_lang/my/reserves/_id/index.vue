aed2b805dfd5a0a81ca2baa6b80fc060cfd8e78c
kenramirezp@gmail.com <kenramirez@Kens-MacBook-Pro.local>
2020-04-20 08:26:25 -0500

    Correccion en detalles de reserva

--- pages/_lang/my/reserves/_id/index.vue
+++ pages/_lang/my/reserves/_id/index.vue
@@ -32,32 +32,30 @@
           <hr class="my-3">
 
           <div class="flex flex-wrap mb-6">
-            <div class="w-1/2 font-light">
-              <p v-if="reserve.experience.host !== ''" class="font-light mb-0">
+            <div v-if="reserve.experience.host !== ''" class="w-1/2 font-light mb-6">
+              <p class="font-light mb-0">
                 <span class="block">Wiser</span>
                 {{ $lang.apiTranslate(reserve.experience.host.translations, 'fullname') }}
               </p>
             </div>
-            <div class="w-1/2 font-light">
+            <div class="w-1/2 font-light mb-6">
               <span class="block">{{ $lang.translate(translations, 'language') }}</span>
               <span>{{ $lang.apiTranslate(reserve.experience.translations, 'languages') }}</span>
             </div>
-          </div>
-          <div class="flex flex-wrap">
-            <div class="w-1/2 font-light">
-              <p v-if="reserve.group_size > 1">
+            <div class="w-1/2 font-light mb-6">
+              <p>
                 <span class="block">{{ $lang.translate(translations, 'group_size') }}</span>
                 <span class="font-light">
                   {{ reserveGroupSize }}
                 </span>
               </p>
             </div>
-            <div class="w-1/2 font-light">
+            <div v-if="reserveExtras" class="w-1/2 font-light mb-6">
               <span class="block">{{ $lang.translate(translations, 'additionals') }}</span>
-              <span>{{ $lang.apiTranslate(reserve.experience.translations, 'carry').join(', ') }}</span>
+              <span>{{ reserveExtras }}</span>
             </div>
           </div>
-          <hr class="my-3">
+          <hr class="-mt-3 mb-3">
           <div class="flex flex-wrap mb-6">
             <div class="w-1/2 font-light">
               <span class="block">{{ $lang.translate(translations, 'destination') }}</span>
@@ -82,7 +80,7 @@
           <div class="w-full">
             <p class="font-light">
               <span class="block">{{ $lang.translate(translations, 'bring') }}</span>
-              <span>Traductor</span>
+              <span>{{ $lang.apiTranslate(reserve.experience.translations, 'carry').join(', ') }}</span>
             </p>
           </div>
         </div>
@@ -157,6 +155,21 @@ export default {
       } else {
         return `${this.reserve.group_size - 1} ${this.$lang.translate(this.translations, 'travelers_you')}`
       }
+    },
+    reserveExtras() {
+      if(!this.reserve.extras) return ''
+      
+      const extras = []
+      this.reserve.extras.forEach(item => {
+        extras.push(this.$lang.apiTranslate(item.translations, 'title'))
+        // if(extras.length === 0) {
+        //   extras.push(this.$lang.translate(item.translations, 'title'))
+        // } else {
+        //   extras.push(', ' + this.$lang.translate(item.translations, 'title'))
+        // }
+      })
+
+      return extras.join(', ')
     }
   },
   mounted() {
